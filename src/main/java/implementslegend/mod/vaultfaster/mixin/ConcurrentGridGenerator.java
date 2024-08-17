10ecc0a0542b8bfd0a04ac34f3f727413cde2c6d
ImplementsLegend <thsmpsns50@gmail.com>
2024-08-17 13:10:32 +0200

    removed use of VH ObjectCache in favor of ConcurrentGridCache

--- src/main/java/implementslegend/mod/vaultfaster/mixin/ConcurrentGridGenerator.java
+++ src/main/java/implementslegend/mod/vaultfaster/mixin/ConcurrentGridGenerator.java
@@ -1,7 +1,10 @@
 package implementslegend.mod.vaultfaster.mixin;
 
 import implementslegend.mod.vaultfaster.ConcurrentGridCache;
+import iskallia.vault.core.Version;
 import iskallia.vault.core.data.key.FieldKey;
+import iskallia.vault.core.event.CommonEvents;
+import iskallia.vault.core.event.common.TemplateGenerationEvent;
 import iskallia.vault.core.random.ChunkRandom;
 import iskallia.vault.core.util.ObjectCache;
 import iskallia.vault.core.util.RegionPos;
@@ -14,28 +17,48 @@ import net.minecraft.world.level.ChunkPos;
 import net.minecraft.world.level.ServerLevelAccessor;
 import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
-import org.spongepowered.asm.mixin.injection.At;
-import org.spongepowered.asm.mixin.injection.Inject;
-import org.spongepowered.asm.mixin.injection.Redirect;
-import org.spongepowered.asm.mixin.injection.callback.CallbackInfo;
-import org.spongepowered.asm.mixin.injection.callback.LocalCapture;
+
+import static iskallia.vault.core.world.generator.GridGenerator.CELL_X;
+import static iskallia.vault.core.world.generator.GridGenerator.CELL_Z;
 
 @Mixin(GridGenerator.class)
 public class ConcurrentGridGenerator {
 
     @Shadow @Final public static FieldKey<iskallia.vault.core.world.generator.layout.GridLayout> LAYOUT;
-    @Shadow protected ObjectCache<RegionPos, ConfiguredTemplate> cache;
     private ConcurrentGridCache concurrentCache = new ConcurrentGridCache();
 
+    @Overwrite(remap = false)
+    public void generate(Vault vault, ServerLevelAccessor world, ChunkPos chunkPos) {
+        BlockPos min = new BlockPos(chunkPos.x * 16, Integer.MIN_VALUE, chunkPos.z * 16);
+        BlockPos max = new BlockPos(chunkPos.x * 16 + 15, Integer.MAX_VALUE, chunkPos.z * 16 + 15);
 
-    @Redirect(method = "generate(Liskallia/vault/core/vault/Vault;Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/level/ChunkPos;)V",at = @At(value = "INVOKE", target = "Liskallia/vault/core/util/ObjectCache;has(Ljava/lang/Object;)Z"),remap = false)
-    private <K> boolean alwaysCached(ObjectCache instance, K object){
-        return true;
-    }
+        var cellX = ((VaultGenerator)(Object)this).get(CELL_X).intValue();
+        var cellZ = ((VaultGenerator)(Object)this).get(CELL_Z).intValue();
+        var versionFlag = vault.get(Vault.VERSION).isOlderThan(Version.v1_7);
+        var layout = ((VaultGenerator)(Object)this).get(LAYOUT);
+        var seed = vault.get(Vault.SEED).longValue();
+        for (var x = min.getX();x <= max.getX();x += cellX - Math.floorMod(x, cellX)) {
+            for (int z = min.getZ(); z <= max.getZ(); z += cellZ - Math.floorMod(z, cellZ)) {
+                RegionPos region = RegionPos.ofBlockPos(new BlockPos(x, 0, z), cellX, cellZ);
+                ChunkRandom random = ChunkRandom.any();
+                if (versionFlag) {
+                    random.setCarverSeed(seed, region.getX(), region.getZ());
+                } else {
+                    random.setRegionSeed(seed, region.getX(), region.getZ(), 1234567890L);
+                }
 
-    @Inject(method = "generate(Liskallia/vault/core/vault/Vault;Lnet/minecraft/world/level/ServerLevelAccessor;Lnet/minecraft/world/level/ChunkPos;)V",at = @At(value = "INVOKE", target = "Liskallia/vault/core/util/ObjectCache;get(Ljava/lang/Object;)Ljava/lang/Object;"),locals = LocalCapture.CAPTURE_FAILEXCEPTION,remap = false)
-    private void concurrentCache(Vault vault, ServerLevelAccessor world, ChunkPos chunkPos, CallbackInfo ci, BlockPos min, BlockPos max, int x, int z, RegionPos region, ChunkRandom random){
-        cache.set(region,concurrentCache.getAt(region,((VaultGenerator)(Object)this).get(LAYOUT),vault,random));
+                ConfiguredTemplate template = concurrentCache.getAt(region,layout,vault,random);
+
+                if (template != null) {
+                    template = CommonEvents.TEMPLATE_GENERATION.invoke(world, template, region, chunkPos, random, TemplateGenerationEvent.Phase.PRE).getTemplate();
+                    template.place(world, chunkPos);
+                    CommonEvents.TEMPLATE_GENERATION.invoke(world, template, region, chunkPos, random, TemplateGenerationEvent.Phase.POST);
+                }
+            }
+
+
+        }
     }
 }
