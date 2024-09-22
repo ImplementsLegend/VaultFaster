18f742edd3a6019a5602e8b59fed56fddcf8612b
ImplementsLegend <thsmpsns50@gmail.com>
2024-09-22 17:39:36 +0200

    renamed file

--- src/main/java/implementslegend/mod/vaultfaster/mixin/ProtoChunkAccessor.kt
+++ src/main/java/implementslegend/mod/vaultfaster/mixin/ProtoChunkAccessor.kt
@@ -1,17 +1,13 @@
-package implementslegend.mod.vaultfaster.mixin;
+package implementslegend.mod.vaultfaster.mixin
 
-import net.minecraft.core.BlockPos;
-import net.minecraft.world.level.chunk.ProtoChunk;
-import net.minecraft.world.level.lighting.LevelLightEngine;
-import org.spongepowered.asm.mixin.Mixin;
-import org.spongepowered.asm.mixin.gen.Accessor;
+import net.minecraft.core.BlockPos
+import net.minecraft.world.level.chunk.ProtoChunk
+import net.minecraft.world.level.lighting.LevelLightEngine
+import org.spongepowered.asm.mixin.Mixin
+import org.spongepowered.asm.mixin.gen.Accessor
 
-import java.util.List;
-
-@Mixin(ProtoChunk.class)
-public interface ProtoChunkAccessor {
-    @Accessor
-    LevelLightEngine getLightEngine();
-    @Accessor
-    List<BlockPos> getLights();
+@Mixin(ProtoChunk::class)
+interface ProtoChunkAccessor {
+    @get:Accessor
+    val lights: MutableList<BlockPos>?
 }
