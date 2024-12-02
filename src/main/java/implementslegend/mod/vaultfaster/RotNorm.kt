6733ecacfabcd5d398a951e4122d60b2213fce94
ImplementsLegend <thsmpsns50@gmail.com>
2024-12-02 21:27:42 +0100

    fixed some buggy things

--- src/main/java/implementslegend/mod/vaultfaster/RotNorm.kt
+++ src/main/java/implementslegend/mod/vaultfaster/RotNorm.kt
@@ -6,9 +6,11 @@ import net.minecraft.world.level.block.Rotation
 data class RotNorm private constructor(val rotation:Int, val invertedNormal:Int,val unused:Unit){
     constructor(rotation:Int,normal:Int):this(rotation and 0b11, normal and 0b1,Unit)
 }
-
 operator fun RotNorm.plus(other: RotNorm): RotNorm =
-    RotNorm(rotation + other.rotation, invertedNormal + other.invertedNormal)
+    RotNorm(rotation + other.rotation
+            +if(invertedNormal == 0 && (rotation and 1)==1 && other.invertedNormal==1)2 else 0//not sure why is this correct but it it correct
+        , invertedNormal + other.invertedNormal)
+
 fun RotateMirror.toRotNorm(): RotNorm = when(mirror){
     Mirror.NONE -> when(rotation){
         Rotation.NONE -> RotNorm(0, 0)
@@ -16,26 +18,26 @@ fun RotateMirror.toRotNorm(): RotNorm = when(mirror){
         Rotation.CLOCKWISE_180 -> RotNorm(2, 0)
         Rotation.COUNTERCLOCKWISE_90 -> RotNorm(3, 0)
     }
-    Mirror.LEFT_RIGHT -> when(rotation){
-        Rotation.NONE -> RotNorm(0, 1)
-        Rotation.CLOCKWISE_90 -> RotNorm(3, 1)
-        Rotation.CLOCKWISE_180 -> RotNorm(2, 1)
-        Rotation.COUNTERCLOCKWISE_90 -> RotNorm(1, 1)
-    }
     Mirror.FRONT_BACK -> when(rotation){
         Rotation.NONE -> RotNorm(2, 1)
         Rotation.CLOCKWISE_90 -> RotNorm(1, 1)
         Rotation.CLOCKWISE_180 -> RotNorm(0, 1)
         Rotation.COUNTERCLOCKWISE_90 -> RotNorm(3, 1)
     }
+    Mirror.LEFT_RIGHT -> when(rotation){
+        Rotation.NONE -> RotNorm(0, 1)
+        Rotation.CLOCKWISE_90 -> RotNorm(3, 1)
+        Rotation.CLOCKWISE_180 -> RotNorm(2, 1)
+        Rotation.COUNTERCLOCKWISE_90 -> RotNorm(1, 1)
+    }
 }
 
 fun RotNorm.toRotateMirror(): RotateMirror = RotateMirror(
-    when (rotation and 0b11) {
+    when (rotation) {
         0 -> Rotation.NONE
         1 -> Rotation.CLOCKWISE_90
         2 -> Rotation.CLOCKWISE_180
         3 -> Rotation.COUNTERCLOCKWISE_90
         else -> throw IllegalStateException("what the hell?")
-    }, if (invertedNormal == 0) Mirror.NONE else if (rotation and 1 == 0) Mirror.LEFT_RIGHT else Mirror.FRONT_BACK
+    }, if (invertedNormal == 0) Mirror.NONE else if ((rotation and 1) == 0) Mirror.LEFT_RIGHT else Mirror.FRONT_BACK
 )
