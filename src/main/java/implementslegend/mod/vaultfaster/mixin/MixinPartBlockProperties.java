b005325fb3bcef7bed39816c8441c6401048656d
ImplementsLegend <thsmpsns50@gmail.com>
2024-12-02 13:13:24 +0100

    more insignificant optimizations

--- src/main/java/implementslegend/mod/vaultfaster/mixin/MixinPartBlockProperties.java
+++ src/main/java/implementslegend/mod/vaultfaster/mixin/MixinPartBlockProperties.java
@@ -1,10 +1,14 @@
 package implementslegend.mod.vaultfaster.mixin;
 
 import iskallia.vault.core.world.data.tile.PartialBlockProperties;
+import org.spongepowered.asm.mixin.Final;
 import org.spongepowered.asm.mixin.Mixin;
+import org.spongepowered.asm.mixin.Shadow;
 import org.spongepowered.asm.mixin.injection.At;
+import org.spongepowered.asm.mixin.injection.Inject;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
 import org.spongepowered.asm.mixin.injection.Redirect;
+import org.spongepowered.asm.mixin.injection.callback.CallbackInfoReturnable;
 
 import java.util.HashMap;
 import java.util.LinkedHashMap;
@@ -15,7 +19,12 @@ import java.util.Map;
 * */
 
 @Mixin(PartialBlockProperties.class)
-public class MixinPartBlockProperties  {
+public abstract class MixinPartBlockProperties  {
+
+    @Shadow @Final private Map<String, String> properties;
+
+    @Shadow public abstract Map<String, String> getProperties();
+
 
     private static final HashMap ANTIALLOCATION = new HashMap();
 
@@ -26,6 +35,12 @@ public class MixinPartBlockProperties  {
     @Redirect(method = "of(Lnet/minecraft/world/level/block/state/BlockState;)Liskallia/vault/core/world/data/tile/PartialBlockProperties;", at = @At(value = "NEW", target = "()Ljava/util/HashMap;"), remap = false)
     private static HashMap useLinkedHashMap2(){
         return ANTIALLOCATION;
+    }
+
+
+    @Inject(method = "copy()Liskallia/vault/core/world/data/tile/PartialBlockProperties;", at = @At("HEAD"), remap = false,cancellable = true)
+    private void useLinkedHashMap3(CallbackInfoReturnable<PartialBlockProperties> cir){
+        cir.setReturnValue(PartialBlockProperties.of(new LinkedHashMap<>(properties)));
     }
 
 
