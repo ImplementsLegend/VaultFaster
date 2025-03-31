2a34321d741abb2ff224d3046de82082e03e2881
ImplementsLegend <thsmpsns50@gmail.com>
2025-04-01 00:00:32 +0200

    refactor: rename misspelled variable

--- src/main/java/implementslegend/mod/vaultfaster/mixin/processor/MixinLeveledProcessor.java
+++ src/main/java/implementslegend/mod/vaultfaster/mixin/processor/MixinLeveledProcessor.java
@@ -1,18 +1,14 @@
 package implementslegend.mod.vaultfaster.mixin.processor;
 
 import implementslegend.mod.vaultfaster.kotlinmixins.processor.KMixinLeveledProcessor;
-import iskallia.vault.core.vault.Vault;
-import iskallia.vault.core.vault.VaultLevel;
 import iskallia.vault.core.world.data.tile.PartialTile;
 import iskallia.vault.core.world.processor.ProcessorContext;
 import iskallia.vault.core.world.processor.tile.LeveledTileProcessor;
 import iskallia.vault.core.world.processor.tile.TileProcessor;
-import iskallia.vault.init.ModConfigs;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.Overwrite;
 import org.spongepowered.asm.mixin.Shadow;
 
-import java.lang.ref.PhantomReference;
 import java.util.Map;
 
 /*
@@ -24,10 +20,10 @@ public class MixinLeveledProcessor {
 
     @Shadow public Map<Integer, TileProcessor> levels;
 
-    private KMixinLeveledProcessor implemntation = new KMixinLeveledProcessor(()->levels);
+    private KMixinLeveledProcessor implementation = new KMixinLeveledProcessor(()->levels);
 
     @Overwrite(remap = false)
     public PartialTile process(PartialTile tile, ProcessorContext context) {
-        return implemntation.process(tile, context);
+        return implementation.process(tile, context);
     }
 }
